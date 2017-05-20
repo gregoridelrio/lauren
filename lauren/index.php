c85767b7321335ba77a41fad2aa79d347f33495c
gregoridelrio <gregoridelrio@gmail.com>
2017-05-20 12:51:39 +0200

    formulario de registro

---
+++ lauren/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Aplicacion</title>
+  </head>
+  <body>
+    <form action="home.php">
+     <div class="container">
+
+       <label><b>Username</b></label>
+       <input type="text" placeholder="Enter Username" name="uname" required>
+
+       <label><b>Password</b></label>
+       <input type="password" placeholder="Enter Password" name="psw" required>
+
+       <button type="submit">Login</button>
+
+     </div>
+    </form>
+
+  </body>
+</html>
