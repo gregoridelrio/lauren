44e48ecf65e9b81916127d438e2dc604d8a14664
gregoridelrio <gregoridelrio@gmail.com>
2017-05-21 17:16:29 +0200

    Captcha añadido (ok)

--- lauren/index.php
+++ lauren/index.php
@@ -3,12 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title>Aplicación</title>
+    <script src='https://www.google.com/recaptcha/api.js'></script>
   </head>
   <body>
     <form action="home.php">
      <div class="container">
      <div>
-     <p>CAPTCHA</p>
      </div>
 
        <label><b>Username</b></label>
@@ -20,6 +20,7 @@
        <button type="submit">Login</button>
 
      </div>
+     <div class="g-recaptcha" data-sitekey="6Ld-VyIUAAAAAFejMEpjvVPPWZkrU_koBlbU9GSR"></div>
     </form>
 
   </body>
